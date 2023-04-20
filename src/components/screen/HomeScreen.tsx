f79d9472d810aca962381928b9e09610fde2736f
Brian-11136015 <131105768+Brian-11136015@users.noreply.github.com>
2023-04-20 12:19:26 +0800

    refactor: 調整資料夾結構

---
+++ src/components/screen/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {Text, View } from 'react-native';
+
+const HomeScreen = () => {
+    return(
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home</Text>
+        </View>
+    );
+};
+export default HomeScreen
---
+++ src/components/screen/PorfileScreen.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {Text, View } from 'react-native';
+
+const PorfileScreen = () => {
+    return(
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Porfile</Text>
+        </View>
+    );
+};
+export default PorfileScreen
