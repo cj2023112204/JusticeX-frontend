447f1dc4b3dd6a61cbbb2809049c05581152175c
brian11136015 <131105768+brian11136015@users.noreply.github.com>
2023-06-01 00:21:26 +0800

    navigation整合、搜尋bar、分頁、判例內容

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,12 +1,13 @@
-import * as React from 'react';
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View, Dimensions } from 'react-native';
 import SearchBar from "react-native-dynamic-search-bar";
 import Article from './Article';
-import { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import CommentList from './Comment';
 import { API_URL } from '../../config';
+
+const { width } = Dimensions.get('window');
 
 const categories = [
   { id: 1, name: '熱門判例' },
@@ -25,10 +26,11 @@ const HomeScreen = () => {
   const [articles, setArticles] = useState<Article[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [isLatest, setIsLatest] = useState(0);
+  const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
     fetchArticles(currentPage, isLatest);
-  }, [isLatest]);
+  }, [currentPage, isLatest]);
 
   const fetchArticles = (page: number, isLatest: number) => {
     const checkLoginStatus = async () => {
@@ -50,17 +52,37 @@ const HomeScreen = () => {
     checkLoginStatus();
   };
 
-  const handleLoadMore = () => {
+  const handleNextPage = () => {
     const nextPage = currentPage + 1;
     fetchArticles(nextPage, isLatest);
     setCurrentPage(nextPage);
   };
 
+  const handlePrevPage = () => {
+    const prevPage = currentPage - 1;
+    fetchArticles(prevPage, isLatest);
+    setCurrentPage(prevPage);
+  };
+
+  const handlePageChange = (pageNumber: number) => {
+    fetchArticles(pageNumber, isLatest);
+    setCurrentPage(pageNumber);
+  };
+
   const handleCategoryPress = (category: any) => {
     setActiveCategory(category);
     setIsLatest(category.id === 2 ? 1 : 0);
-    setArticles([]); // 清空articles数组
+    setArticles([]);
+    setCurrentPage(1); // 重置为第一页
   };
+
+  const handleSearch = (text: string) => {
+    setSearchText(text);
+  };
+
+  const filteredArticles = articles.filter(article =>
+    article.title.toLowerCase().includes(searchText.toLowerCase())
+  );
 
   const renderArticleItem = ({ item }: { item: Article }) => {
     return (
@@ -73,11 +95,81 @@ const HomeScreen = () => {
     );
   };
 
+  const renderPagination = () => {
+    if (filteredArticles.length > 0) {
+      const totalPages = Math.ceil(filteredArticles.length / 10); // 假设每页显示10个文章
+      const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
+      const visiblePageNumbers = pageNumbers.slice(Math.max(currentPage - 2, 0), currentPage + 3);
+
+      const renderPageNumbers = visiblePageNumbers.map(pageNumber => (
+        <TouchableOpacity
+          key={pageNumber}
+          style={[
+            styles.paginationButton,
+            currentPage === pageNumber && styles.activePaginationButton,
+            styles.pageNumberButton,
+          ]}
+          onPress={() => handlePageChange(pageNumber)}
+        >
+          <Text
+            style={[
+              styles.paginationButtonText,
+              currentPage === pageNumber && styles.activePaginationButtonText,
+              styles.pageNumberText,
+            ]}
+          >
+            {pageNumber}
+          </Text>
+        </TouchableOpacity>
+      ));
+
+      return (
+        <View style={styles.paginationContainer}>
+          {currentPage > 1 && (
+            <TouchableOpacity
+              style={[styles.paginationButton, styles.pageNumberButton]}
+              onPress={handlePrevPage}
+            >
+              <Text style={[styles.paginationButtonText, styles.pageNumberText]}>Prev</Text>
+            </TouchableOpacity>
+          )}
+          {visiblePageNumbers[0] > 1 && (
+            <TouchableOpacity
+              style={styles.paginationButton}
+              onPress={() => handlePageChange(visiblePageNumbers[0] - 1)}
+            >
+              <Text style={styles.paginationButtonText}>...</Text>
+            </TouchableOpacity>
+          )}
+          {renderPageNumbers}
+          {visiblePageNumbers[visiblePageNumbers.length - 1] < totalPages && (
+            <TouchableOpacity
+              style={styles.paginationButton}
+              onPress={() => handlePageChange(visiblePageNumbers[visiblePageNumbers.length - 1] + 1)}
+            >
+              <Text style={styles.paginationButtonText}>...</Text>
+            </TouchableOpacity>
+          )}
+          {currentPage < totalPages && (
+            <TouchableOpacity
+              style={[styles.paginationButton, styles.pageNumberButton]}
+              onPress={handleNextPage}
+            >
+              <Text style={[styles.paginationButtonText, styles.pageNumberText]}>Next</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+      );
+    }
+    return null;
+  };
+
   return (
     <View style={styles.container}>
       <SearchBar
         placeholder="Search here"
-        onChangeText={(text) => console.log(text)}
+        onChangeText={handleSearch}
+        style={styles.searchbar}
       />
       <View style={styles.categoryBar}>
         {categories.map(category => (
@@ -85,6 +177,7 @@ const HomeScreen = () => {
             key={category.id}
             style={[
               styles.categoryButton,
+              { width: width / categories.length },
               activeCategory.id === category.id && styles.activeCategoryButton,
             ]}
             onPress={() => handleCategoryPress(category)}
@@ -101,23 +194,23 @@ const HomeScreen = () => {
         ))}
       </View>
       <FlatList
-        data={articles}
+        data={filteredArticles}
         renderItem={renderArticleItem}
         keyExtractor={(item, index) => `${item.title}-${index}`}
-        onEndReached={handleLoadMore}
-        onEndReachedThreshold={0.5}
       />
+      {renderPagination()}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: '#fff',
   },
   searchbar: {
     height: 56,
-    width: 359,
+    width: '100%',
   },
   categoryBar: {
     flexDirection: 'row',
@@ -141,6 +234,36 @@ const styles = StyleSheet.create({
   },
   activeCategoryButtonText: {
     color: 'black',
+  },
+  paginationContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  paginationButton: {
+    backgroundColor: '#eee',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginHorizontal: 5,
+    borderRadius: 5,
+  },
+  activePaginationButton: {
+    backgroundColor: 'black',
+  },
+  paginationButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  pageNumberButton: {
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  pageNumberText: {
+    color: 'white',
   },
 });
 
