88dfd516da9ddd7ad023a9ef48fca6553daca5ff
sharron <10656035@ntub.edu.tw>
2023-05-31 11:00:21 +0800

    更新個人資料，新增更改頭貼頁面

--- src/screen/ProfileScreen.tsx
+++ src/screen/ProfileScreen.tsx
@@ -1,136 +1,100 @@
 import * as React from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import MaterIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-import FontIcon from 'react-native-vector-icons/FontAwesome';
-
 import HomeScreen from "./HomeScreen";
+import ChangeAvatar from "./ChangeAvatarScreen";
+import { useNavigation } from '@react-navigation/native';
 
 const PorfileScreen = () => {
   const avatarSource = require('./assets/images/photo.jpg'); // 替换为实际的头像图片路径
   const name = 'John Doe'; // 替换为实际的姓名数据
 
-  const handleLogout = () => {
-    // 执行登出操作
-    // 清除用户凭据、导航到登录页面等
-    console.log('点击了登出按钮');
-  };
 
   const handleListItemPress = (listItemName: string) => {
     // 处理列表项点击事件
     console.log(`点击了列表项: ${listItemName}`);
+
   };
 
   return (
-    <View style={styles.container}>
-      <Image source={avatarSource} style={styles.avatar} />
-      <Text style={styles.name}>{name}</Text>
+    <View>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={ChangeAvatar}>
+          <Image source={avatarSource} style={styles.avatar} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.name} onPress={HomeScreen}>
+          <Text style={styles.name}>{name}</Text>
+        </TouchableOpacity>
+      </View>
 
       <View style={styles.separator}></View>
 
-      <TouchableOpacity style={styles.button} onPress={HomeScreen}>
-      <Image source={avatarSource} style={styles.avatarButton} />
-        <Text style={styles.buttonText}>profile</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.forgotPasswordButton} onPress={HomeScreen}>
+          <Text style={styles.TextTitle}>性別</Text>
+          <Text style={styles.Text}>女</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.forgotPasswordButton} onPress={HomeScreen}>
+          <Text style={styles.TextTitle}>職業</Text>
+          <Text style={styles.Text}>教師</Text>
+        </TouchableOpacity>
+
+        <View style={styles.separator}></View>
+      </View>
+
+      <View style={styles.buttonContainer}></View>
+      <TouchableOpacity style={styles.forgotPasswordButton} onPress={HomeScreen}>
+        <Text style={styles.TextTitle}>忘記密碼</Text>
       </TouchableOpacity>
 
-      {/* <TouchableOpacity style={styles.button} onPress={HomeScreen}>
-        <MaterIcon name="onepassword" style={styles.buttonIcon} />
-        <Text style={styles.buttonText}>paassword</Text>
-      </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button} onPress={HomeScreen}>
-        <FontIcon name="bookmark" style={styles.buttonIcon} />
-        <Text style={styles.buttonText}>bookmark</Text>
-      </TouchableOpacity> */}
-
-      <View style={styles.separator}></View>
-
-      <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
-        <Text style={styles.logoutButtonText}>登出</Text>
-      </TouchableOpacity>
-    </View>
+    </View >
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexDirection: 'row',
     alignItems: 'center',
+    padding: 20,
     justifyContent: 'center',
+
+  },
+  Text: {
+    color: 'grey',
+    fontSize: 16,
+    marginTop: 15,
   },
   avatar: {
     width: 100,
     height: 100,
-    borderRadius: 50,
-    marginBottom: 20,
-  },
-  avatarButton: {
-    width: 30,
-    height: 30,
-    borderRadius: 0,
-    marginBottom: 10,
-    padding: 10,
-  },
-  button: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 10,
-    backgroundColor: 'lightblue',
-    borderRadius: 5,
-    marginBottom: 10,
-  },
-  buttonIcon: {
-    marginRight: 5,
-    fontSize: 20,
-    color: 'white',
-  },
-  buttonText: {
-    fontSize: 16,
-    color: 'white',
+    borderRadius: 25,
   },
   name: {
-    fontSize: 24,
+    marginLeft: 10,
+    fontSize: 25,
     fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 5,
-    marginLeft: 20,
-    marginRight: 20,
-  },
-  rowInner: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  label: {
-    fontSize: 16,
-  },
-  arrowIcon: {
-    fontSize: 20,
-    marginLeft: 5,
-  },
-  icon: {
-    marginLeft: 5,
-  },
-  iconContainer: {
-    marginLeft: 5,
-  },
-  logoutButton: {
-    padding: 10,
-  },
-  logoutButtonText: {
-    fontSize: 18,
-    color: 'black',
   },
   separator: {
     height: 3,
-    width: 300,
+    width: 350,
     backgroundColor: 'black',
     marginHorizontal: 20,
     marginTop: 10,
     marginBottom: 10,
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  forgotPasswordButton: {
+    width: 350,
+    paddingVertical: 10,
+
+  },
+  TextTitle: {
+    color: 'black',
+    fontSize: 16,
   },
 });
 export default PorfileScreen;
