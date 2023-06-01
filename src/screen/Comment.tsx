0e64f368d66187ba45ef6d59fd45d96e5e36d1ef
brian11136015 <131105768+brian11136015@users.noreply.github.com>
2023-06-02 01:31:38 +0800

    feat:按讚、回覆、書籤

--- src/screen/Comment.tsx
+++ src/screen/Comment.tsx
@@ -19,6 +19,10 @@ const VerdictScreen = () => {
   const verdictId = route.params?.verdictId; // Access the verdictId parameter from the route
   const [incidentType, setIncidentType] = useState('incident');
   const [verdictData, setVerdictData] = useState(null);
+  const [likesCount, setLikesCount] = useState(0);
+  const [commentsCount, setCommentsCount] = useState(0);
+  const [liked, setLiked] = useState(false); // Add the 'liked' state variable
+  const [bookmarked, setBookmarked] = useState(false);
 
   useEffect(() => {
     fetchVerdictData();
@@ -34,6 +38,8 @@ const VerdictScreen = () => {
       .then((response) => response.json())
       .then((responseData) => {
         setVerdictData(responseData.data);
+        setLikesCount(responseData.data.total_like);
+        setCommentsCount(responseData.data.total_comment);
       })
       .catch((error) => {
         console.error(error);
@@ -42,6 +48,84 @@ const VerdictScreen = () => {
 
   const handleToggleIncidentType = () => {
     setIncidentType(incidentType === 'incident' ? 'incident_lite' : 'incident');
+  };
+
+  const handleCommentSubmit = async (comment) => {
+    try {
+      const accessToken = await AsyncStorage.getItem('access_token');
+      const response = await fetch(`${API_URL}/comment/add_comment/`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          verdict_id: verdictId,
+          content: comment,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      // Refresh comments
+      fetchComments();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleLikeVerdict = async () => {
+    try {
+      const accessToken = await AsyncStorage.getItem('access_token');
+      const url = liked ? `${API_URL}/verdict/unlike_verdict/` : `${API_URL}/verdict/like_verdict/`;
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          verdict_id: verdictId,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      // Refresh likes count
+      fetchVerdictData();
+
+      if (data.is_liked) {
+        // Like was successful
+        setLiked(true);
+      } else {
+        // Like was canceled
+        setLiked(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleBookmarkVerdict = async () => {
+    try {
+      const accessToken = await AsyncStorage.getItem('access_token');
+      const url = `${API_URL}/verdict/collect_verdict/`;
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          verdict_id: verdictId,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+
+      // Update bookmarked state
+      setBookmarked(true);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   if (!verdictData) {
@@ -53,7 +137,7 @@ const VerdictScreen = () => {
   }
 
   return (
-    <ScrollView style={styles.container}>
+    <View style={styles.container}>
       <View style={styles.verdictContainer}>
         <Text style={styles.verdictTitle}>{verdictData.title}</Text>
         <Text style={styles.verdictData}>{verdictData[incidentType]}</Text>
@@ -63,14 +147,29 @@ const VerdictScreen = () => {
 
       <TouchableOpacity onPress={handleToggleIncidentType} style={styles.toggleButton}>
         <Text style={styles.toggleButtonText}>
-          {incidentType === 'incident' ? '切換案件類型' : 'Switch Incident Type'}
+          {incidentType === 'incident' ? '查看簡化判例' : '查看原始判例'}
         </Text>
       </TouchableOpacity>
 
       <View style={styles.separator}></View>
 
-      <CommentList verdictId={verdictId} />
-    </ScrollView>
+      <View style={styles.likesCommentsContainer}>
+        <TouchableOpacity onPress={handleLikeVerdict}>
+          <Text style={[styles.likeButton, liked ? { color: 'white' } : null]}>❤️ Like</Text>
+        </TouchableOpacity>
+        <Text style={styles.likesCount}>{likesCount} likes</Text>
+        <Text style={styles.commentsCount}>{commentsCount} comments</Text>
+        <TouchableOpacity onPress={handleBookmarkVerdict}>
+          <Text style={styles.bookmarkButton}>
+            {bookmarked ? '🔖' : '🔖'}
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.separator}></View>
+
+      <CommentList verdictId={verdictId} handleCommentSubmit={handleCommentSubmit} />
+    </View>
   );
 };
 
@@ -83,8 +182,8 @@ const CommentItem = ({ comment }) => {
       <View style={styles.commentContent}>
         <Text style={styles.commentText}>{comment.comment}</Text>
         <View style={styles.commentInfoContainer}>
-          <Text style={styles.commentInfoText}>{`By: ${comment.commenter}`}</Text>
-          <Text style={styles.commentInfoText}>{`Time: ${comment.time}`}</Text>
+          <Text style={styles.commentInfoText}>{`By: ${comment.comment_email}`}</Text>
+          <Text style={styles.commentInfoText}>{`Time: ${comment.comment_create_time}`}</Text>
         </View>
         <View style={styles.likeDislikeContainer}></View>
         <Button title="Reply" />
@@ -106,28 +205,36 @@ const CommentItem = ({ comment }) => {
   );
 };
 
-const CommentList = ({ verdictId }) => {
+const CommentList = ({ verdictId, handleCommentSubmit }) => {
   const navigation = useNavigation();
   const [comments, setComments] = useState([]);
+  const [commentText, setCommentText] = useState('');
 
   useEffect(() => {
-    const fetchComments = async () => {
-      try {
-        const accessToken = await AsyncStorage.getItem('access_token');
-        const response = await fetch(`${API_URL}/comment/get_comments/?verdict_id=${verdictId}`, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-        const data = await response.json();
-        setComments(data.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
     fetchComments();
   }, []);
+
+  const fetchComments = async () => {
+    try {
+      const accessToken = await AsyncStorage.getItem('access_token');
+      const response = await fetch(`${API_URL}/comment/get_comments/?verdict_id=${verdictId}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const data = await response.json();
+      setComments(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSendComment = () => {
+    if (commentText.trim() !== '') {
+      handleCommentSubmit(commentText);
+      setCommentText('');
+    }
+  };
 
   return (
     <View>
@@ -137,8 +244,13 @@ const CommentList = ({ verdictId }) => {
         keyExtractor={(item) => item.comment_id.toString()}
       />
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} placeholder="Enter comment..." />
-        <Button title="Send" onPress={() => {}} />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter comment..."
+          value={commentText}
+          onChangeText={setCommentText}
+        />
+        <Button title="Send" onPress={handleSendComment} />
       </View>
     </View>
   );
@@ -181,6 +293,23 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textDecorationLine: 'underline',
   },
+  likesCommentsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  likeButton: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginRight: 8,
+  },
+  likesCount: {
+    fontSize: 16,
+    marginRight: 8,
+  },
+  commentsCount: {
+    fontSize: 16,
+  },
   commentContainer: {
     flexDirection: 'row',
     marginBottom: 10,
@@ -213,12 +342,10 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   repliesContainer: {
-    marginTop: 10,
+    marginLeft: 20,
   },
   replyContainer: {
-    backgroundColor: '#f0f0f0',
-    padding: 10,
-    marginBottom: 5,
+    marginBottom: 10,
   },
   replyText: {
     fontSize: 14,
@@ -226,7 +353,7 @@ const styles = StyleSheet.create({
   },
   replyInfoContainer: {
     flexDirection: 'row',
-    marginTop: 5,
+    marginBottom: 5,
   },
   replyInfoText: {
     fontSize: 12,
@@ -235,15 +362,21 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     flexDirection: 'row',
-    marginTop: 10,
+    marginBottom: 16,
   },
   input: {
     flex: 1,
-    height: 40,
     borderWidth: 1,
-    borderColor: 'gray',
-    marginRight: 10,
-    paddingHorizontal: 10,
+    borderColor: '#CCCCCC',
+    borderRadius: 8,
+    marginRight: 8,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+  },
+  bookmarkButton: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginLeft: 100,
   },
 });
 
