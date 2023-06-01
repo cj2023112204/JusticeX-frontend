7f9b0d2f28b3c52e4556bc5fd2512df04f5d8f9b
sharron <10656035@ntub.edu.tw>
2023-06-02 01:30:02 +0800

    更改密碼 連接後端(寫死)

--- src/screen/ChangePasswordScreen.tsx
+++ src/screen/ChangePasswordScreen.tsx
@@ -1,16 +1,57 @@
 import { View, Text,TextInput, StyleSheet, ScrollView } from 'react-native';
 import React, { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
 import CustomButton from '../components/CustomButton';
 import CustomInput from '../components/CustomInput';
-const ChangePprofileScreen = () => {
-  const [email, setEmail] = useState('');
+import { API_URL } from '../../config';
 
-  const navigation = useNavigation();
+const ChangePprofileScreen = () => {
+
+  // const navigation = useNavigation();
 
   //const API_URL = 'http://13.208.146.112:8000/api';
 
+  const [email, setname] = useState('11136018@ntub.edu.tw');
+  const [password, setPassword] = useState('11136018');
+  const navigation = useNavigation();
   
+  //const API_URL = 'http://13.208.146.112:8000/api';
+
+  const onChangePw = async() => {
+      // console.warn('onSubmitPressed');
+      const accessToken = await AsyncStorage.getItem('access_token');
+      fetch(`${API_URL}/account/change_password/`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            // Handle response data
+            //setUsername(data)
+            //setPassword(data)
+            if(data.success===true){
+              console.log(data.message)
+            }else{
+              console.log('error')
+            }
+            console.log(data)
+            navigation.goBack();
+          })
+          .catch((error) => {
+            // Handle error
+            console.error(error);
+          });
+
+      //navigation.navigate('SignIn' as never);
+  };
   const onPressed = () => {
     navigation.goBack();
   };
@@ -20,12 +61,22 @@ const ChangePprofileScreen = () => {
       <View style={styles.root}>
         <Text style={styles.title}>Reset your change password</Text>
 
-        <View style={styles.container}>
+        {/* <View style={styles.container}>
             <TextInput placeholder="new password"/>
-        </View>
-        <View style={styles.container}>
+        </View> */}
+        <CustomInput
+          placeholder="new password"
+          value={password}
+          setValue={setPassword}
+        />
+        <CustomInput
+          placeholder="reconfirm password"
+          value={password}
+          setValue={setPassword}
+        />
+        {/* <View style={styles.container}>
             <TextInput placeholder="reconfirm password"/>
-        </View>
+        </View> */}
         {/* <CustomInput
           placeholder="name"
           value={email}
@@ -42,7 +93,7 @@ const ChangePprofileScreen = () => {
           setValue={setEmail}
         /> */}
 
-        <CustomButton text="Send" onPress={onPressed} />
+        <CustomButton text="Send" onPress={onChangePw} />
 
         <CustomButton
           text="Back to Profile"
