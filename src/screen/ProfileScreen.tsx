eff0e87fbaf6c12cf887ec8383ed8f262498ed1f
sharron <10656035@ntub.edu.tw>
2023-06-01 10:46:13 +0800

    更新個人資料和更改頭貼頁面

--- src/screen/ProfileScreen.tsx
+++ src/screen/ProfileScreen.tsx
@@ -1,27 +1,29 @@
 import * as React from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import HomeScreen from "./HomeScreen";
-import ChangeAvatar from "./ChangeAvatarScreen";
 import { useNavigation } from '@react-navigation/native';
 
 const PorfileScreen = () => {
-  const avatarSource = require('../../assets/images/photo.jpg'); // 替换为实际的头像图片路径
+  const avatarSource = require('../../assets/images/1.jpg'); // 替换为实际的头像图片路径
   const name = 'John Doe'; // 替换为实际的姓名数据
 
-
-  const handleListItemPress = (listItemName: string) => {
-    // 处理列表项点击事件
-    console.log(`点击了列表项: ${listItemName}`);
-
+  const navigation = useNavigation();
+  const onPressed = () => {
+    navigation.navigate('Favorite' as never); // Remove unnecessary type casting
+  };
+  const changeavatar = () => {
+    navigation.navigate('ChangeAvatar' as never); // Remove unnecessary type casting
+  };
+  const changepprofile = () => {
+    navigation.navigate('ChangePprofile' as never); // Remove unnecessary type casting
   };
 
   return (
     <View>
       <View style={styles.container}>
-        <TouchableOpacity onPress={ChangeAvatar}>
+        <TouchableOpacity onPress={changeavatar}>
           <Image source={avatarSource} style={styles.avatar} />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.name} onPress={HomeScreen}>
+        <TouchableOpacity style={styles.name} onPress={changepprofile}>
           <Text style={styles.name}>{name}</Text>
         </TouchableOpacity>
       </View>
@@ -29,24 +31,24 @@ const PorfileScreen = () => {
       <View style={styles.separator}></View>
 
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.forgotPasswordButton} onPress={HomeScreen}>
-          <Text style={styles.TextTitle}>性別</Text>
-          <Text style={styles.Text}>女</Text>
+        <TouchableOpacity style={styles.button} onPress={changepprofile}>
+          <Text style={styles.TextTitle}>gender</Text>
+          <Text style={styles.Text}>F</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.forgotPasswordButton} onPress={HomeScreen}>
-          <Text style={styles.TextTitle}>職業</Text>
-          <Text style={styles.Text}>教師</Text>
+        <TouchableOpacity style={styles.button} onPress={changepprofile}>
+          <Text style={styles.TextTitle}>job_name</Text>
+          <Text style={styles.Text}>Teacher</Text>
         </TouchableOpacity>
 
         <View style={styles.separator}></View>
       </View>
 
-      <View style={styles.buttonContainer}></View>
-      <TouchableOpacity style={styles.forgotPasswordButton} onPress={HomeScreen}>
-        <Text style={styles.TextTitle}>忘記密碼</Text>
-      </TouchableOpacity>
-
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.favoriteButton} onPress={onPressed}>
+          <Text style={styles.TextTitle}>favorite</Text>
+        </TouchableOpacity>
+      </View>
 
     </View >
   );
@@ -87,10 +89,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  forgotPasswordButton: {
+  button: {
     width: 350,
     paddingVertical: 10,
-
+  },
+  favoriteButton: {
+    width: 350,
+    paddingVertical: 10,
   },
   TextTitle: {
     color: 'black',
